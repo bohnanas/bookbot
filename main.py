3f6984ed0bdf80c934c04094f411c9f8c6e92e41
bohnanas <maxwellbohn1997@gmail.com>
2025-02-23 18:48:37 -0500

    added word counting capability

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ def get_book_text(file):
     
     return content
 
+def word_counter(string):
+    words = string.split()
+
+    return len(words)
+
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+
+    num_of_words = word_counter(text)
+    print(f"{num_of_words} words found in the document")
 
 main()
