34562c2bab6a94adac39543d10481308f45b70f3
bohnanas <maxwellbohn1997@gmail.com>
2025-02-23 20:18:51 -0500

    added stats.py and char counting capability

---
+++ stats.py
@@ -0,0 +1,24 @@
+# function to count the number of words in a string
+def word_counter(string):
+    words = string.split()
+
+    return len(words)
+
+# function that counts the number of times each character appears in a string
+def char_counter(string):
+    # create empty dictionary for each char
+    chars = {}
+
+    # iterate over each char in the string
+    for char in string:
+        # if uppercase char, make it lowercase
+        if char.isupper():
+            char = char.lower()
+        # check to see if char matches in dictionary
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+
+    return chars
+
